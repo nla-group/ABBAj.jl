3df083958cf9b532c10aa82f035e591db3f7be5b
Null <42580530+chenxinye@users.noreply.github.com>
2021-11-16 12:14:16 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,5 +13,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/nla-group/ABBAj.git"
+    repo = "github.com/nla-group/ABBAj.jl.git"
 )
